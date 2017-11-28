93afabaaf8bbcd256edb334f250e41115b6e47de
gltknbtn <gltknbtn@gmail.com>
2017-11-28 23:40:55 +0300

    forms operations

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,6 +14,11 @@ import {LoginControl2} from './pages/7_conditionalRendering.js';
 import {Page} from './pages/7_conditionalRendering.js';
 import {NumberList} from './pages/8_listAndKeys.js';
 import {Blog} from './pages/8_listAndKeys.js';
+import {NameForm} from './pages/9_forms.js';
+import {EssayForm} from './pages/9_forms.js';
+import {FlavorForm} from './pages/9_forms.js';
+import {Reservation} from './pages/9_forms.js';
+
 
 
 class App extends Component {
@@ -21,13 +26,22 @@ class App extends Component {
     super(props);
     this.state = {
       numbers : [1, 2, 3, 4, 5, 6],
+      count: 0,
       posts: [
         {id:1, title:'Hello World', content: 'Welcome to learning React'},
         {id:2, title:'Installation', content: 'You can install react from npm.'}
       ]
     };
+
+  this.clickMe = this.clickMe.bind(this);
   }
-  
+
+  clickMe(){
+    this.setState(prevState=>({
+      count: prevState.count + 1
+    }));
+  }
+
   render() {
     return (
       <div className="App">
@@ -44,33 +58,33 @@ class App extends Component {
         <HelloWorld/>
         <br/>
         ====================helloworld end====================<br/><br/>
-        
+
         ====================introducingJSX start====================<br/><br/>
         <br/>
         <IntroducingJsx/>
         <br/>
         ====================introducingJsx end====================<br/><br/>
-        
-        
+
+
         ====================renderingElements start====================<br/><br/>
         <br/>
         <RenderingElements/>
         <br/>
         ====================renderingElements end====================<br/><br/>
-        
+
         ====================componentsAndProps start====================<br/><br/>
         <br/>
         <ComponentsAndProps/>
         <br/>
         ====================componentsAndProps end====================<br/><br/>
-        
+
         ====================stateAndLifeCycle start====================<br/><br/>
         <br/>
-        <StateAndLifeCycle gltkn ="yes gltkn prpos"/>
-        <StateAndLifeCycle gltkn ="yes gltkn prpos"/>
+        <StateAndLifeCycle gltkn ="yes gltkn prpos" count={this.state.count}/>
+        <button onClick={this.clickMe}>clickMe </button>
         <br/>
         ====================stateAndLifeCycle end====================<br/><br/>
-        
+
         ====================handlingEvents start====================<br/><br/>
         <br/>
         <Toggle/>
@@ -79,31 +93,46 @@ class App extends Component {
         <br/>
 
         ====================handlingEvents end====================<br/><br/>
-        
+
         ====================conditionalRendering1 start====================<br/><br/>
         <LoginControl1/>
 
         ====================conditionalRendering1 end====================<br/><br/>
-        
+
         ====================conditionalRendering2 start====================<br/><br/>
         <LoginControl2/>
 
         ====================conditionalRendering2 end====================<br/><br/>
-        
+
         ====================conditionalRendering3 start====================<br/><br/>
         <Page />
         ====================conditionalRendering3 end====================<br/><br/>
-        
+
         ====================listAndKeys1 start====================<br/><br/>
         <NumberList numbers={this.state.numbers} />
         ====================listAndKeys1 end====================<br/><br/>
-        
+
         ====================listAndKeys1 start====================<br/><br/>
         <Blog posts={this.state.posts} />
         ====================listAndKeys1 end====================<br/><br/>
-        
+
+        ====================forms_1 start====================<br/><br/>
+        <NameForm />
+        ====================forms_1 end====================<br/><br/>
+
+        ====================forms_2 start====================<br/><br/>
+        <EssayForm />
+        ====================forms_2 end====================<br/><br/>
+
+        ====================forms_3 start====================<br/><br/>
+        <FlavorForm />
+        ====================forms_3 end====================<br/><br/>
+        ====================forms_4 start====================<br/><br/>
+        <Reservation />
+        ====================forms_4 end====================<br/><br/>
+
       </div>
-      
+
     );
   }
 }
--- my-app/src/pages/5_stateAndLifeCycle.js
+++ my-app/src/pages/5_stateAndLifeCycle.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 
 
 class StateAndLifeCycle extends Component {
-  
+
   constructor(props){
     super(props);
-    console.log("props gltkn: " + props.gltkn);
     this.state = {date: new Date()};
   }
-  
+
+  componentWillReceiveProps(newProps) {
+    console.log('Component WILL RECIEVE PROPS!')
+    console.log("count: "+ newProps.count);
+
+ }
+
   componentDidMount() {
     console.log("componentDidMount");
+      console.log("props gltkn: " + this.props.gltkn);
     this.timerID = setInterval(
       ()=>this.tick(),
       1000
@@ -21,21 +27,21 @@ class StateAndLifeCycle extends Component {
     console.log("componentWillUnmount");
     clearInterval(this.timerID);
   }
-  
+
   tick(){
     this.setState({
       date: new Date()
     });
   }
-  
+
   render() {
     return (
       <div>
-      <div>
-        <h1> Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        {this.props.gltkn}
-      </div>
+        <div>
+          <h1> Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+          {this.props.gltkn}
+        </div>
       </div>
       );
   }
